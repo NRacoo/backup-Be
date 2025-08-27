1d7c0882b70517987ae9e0bfcf7720a776be91d7
NRacoo <ariaaurar@gmail.com>
2025-08-27 12:14:54 +0700

    add

--- src/server.js
+++ src/server.js
@@ -3,16 +3,12 @@ import dotenv from "dotenv";
 import { prisma } from "./config/database.js";
 import app from "./app.js";
 import { initializeSocket } from "./services/socket.service.js";
-import { initializeMqtt, disconnectMqtt } from "./services/mqtt.service.js";
 import { startScheduler, stopScheduler } from "./services/scheduler.service.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 2000;
 const server = http.createServer(app);
-
-initializeSocket(server);
-initializeMqtt();
 startScheduler();
 
 process.on("SIGINT", async () => {
